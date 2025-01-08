27bd04b30824af5ef037dbdb7718394023ff3c72
bw-99 <bb1702@g.skku.edu>
2025-01-08 22:38:15 +0900

    add: reply tag in chat interface

--- service/static/js/paper.js
+++ service/static/js/paper.js
@@ -3,6 +3,10 @@ var socket = io();
 function sendMessage() {
     var input = document.getElementById('message_input');
     var message = input.value;
+    var selectedText = window.getSelection().toString();
+    if (selectedText) {
+        message = `"${selectedText}"\n\n${message}`;
+    }
     console.log('Sending message:', message);
     socket.send(message);
     addMessageToChat(message, 'user');
@@ -23,10 +27,56 @@ function addMessageToChat(message, sender) {
     messages.scrollTop = messages.scrollHeight;
 }
 
+function showReplyButton(event) {
+    var selectedText = window.getSelection().toString();
+    var replyButton = document.getElementById('reply_button');
+    if (selectedText) {
+        replyButton.style.display = 'block';
+        replyButton.style.left = event.pageX + 'px';
+        replyButton.style.top = event.pageY + 'px';
+    } else {
+        replyButton.style.display = 'none';
+    }
+}
+
+function handleIframeSelection() {
+    var iframe = document.getElementById('pdf_iframe');
+    iframe.onload = function() {
+        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+        iframeDocument.addEventListener('mouseup', function(event) {
+            var selectedText = iframeDocument.getSelection().toString();
+            if (selectedText) {
+                var replyButton = document.getElementById('reply_button');
+                replyButton.style.display = 'block';
+                var rect = iframe.getBoundingClientRect();
+                replyButton.style.left = rect.left + event.clientX + 'px';
+                replyButton.style.top = rect.top + event.clientY + 'px';
+            } else {
+                document.getElementById('reply_button').style.display = 'none';
+            }
+        });
+    };
+}
+
 document.getElementById('send_button').addEventListener('click', sendMessage);
+document.addEventListener('mouseup', showReplyButton);
+
+document.getElementById('reply_button').addEventListener('click', function() {
+    var selectedText = window.getSelection().toString();
+    if (!selectedText) {
+        var iframe = document.getElementById('pdf_iframe');
+        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        selectedText = iframeDocument.getSelection().toString();
+    }
+    var input = document.getElementById('message_input');
+    input.value = `"${selectedText}"\n\n` + input.value;
+    document.getElementById('reply_button').style.display = 'none';
+});
 
 socket.on('connect', function() {
     console.log('Connected to server');
+    handleIframeSelection();
 });
 
 socket.on('message', function(msg) {
